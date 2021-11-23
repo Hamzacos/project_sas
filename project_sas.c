a457545ff5377b487cc5b7c6a698e4c58673412d
hamzacos <hamza.laqraa@hotmail.com>
2021-11-23 14:18:56 +0100

    Done

--- project_sas.c
+++ project_sas.c
@@ -6,10 +6,11 @@ typedef struct{
     char prenom[20];
     float montant;
 }Compte;
+int num = 0;
 /*********************fonction Ajoute *******************/
 void ajoute_client(Compte T[])
 {
-        int i= 0;
+        int i= num;
         printf("CIN     :");
         scanf("%s",T[i].CIN);
         printf("Nom     :");
@@ -30,7 +31,7 @@ void ajoute_plusieur_client(Compte T[],int nb)
         printf("entrez le nombre de compte bancaire");
         scanf("%d",&nb);
         int c=1;
-        for(i=0;i<nb;i++)
+        for(i=num;i<nb+num;i++)
     {
         printf("Copmte N%d: \n",c);c++;
         printf("CIN     :");
@@ -46,6 +47,7 @@ void ajoute_plusieur_client(Compte T[],int nb)
         printf("Prenom  : %s \n",T[i].prenom);
         printf("Montant : %.2f \n",T[i].montant);*/
     }
+    num = num + nb;
 }
  /********************fonction recherche ****************/
  int recherche(Compte T[],int n)
@@ -90,10 +92,85 @@ void ajoute_plusieur_client(Compte T[],int nb)
          printf("le solde apres le depot est:%.2f",T[i].montant);
      }
  }
+ /************************Affichage ***********************/
+ void affichCompte(Compte T[],int n)
+ {
+    int c=1;
+    for(int i=0;i<n;i++)
+    {
+        printf("Copmte N%d: \n",c);c++;
+        printf("CIN     : %s \n",T[i].CIN);
+        printf("Nome    : %s \n",T[i].nom);
+        printf("Prenom  : %s \n",T[i].prenom);
+        printf("Montant : %.2f \n",T[i].montant);
+    }
+}
+ void triA(Compte T[],int n)
+ {
+     Compte temp;
+     int i,j;
+     for(i=0;i<n;i++)
+         for(j=0;j<n;j++)
+             if(T[i].montant<T[j].montant)
+             {
+                 temp = T[i];
+                 T[i] = T[j];
+                 T[j] = temp;
+             }
+ }
+ void triD(Compte T[],int n)
+ {
+     Compte temp;
+     int i,j;
+     for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            if(T[i].montant>T[j].montant)
+            {
+                temp = T[i];
+                T[i] = T[j];
+                T[j] = temp;
+            }
+ }
+ void RtriA(Compte T[])
+ {
+    Compte temp[20];
+    int i, j = 0;
+    float nb ;
+    printf("enter le chiffre pivot: ");
+    scanf("%f",&nb);
+    for(i=0;i<num;i++)
+    {
+      if(T[i].montant >= nb)
+      {
+        temp[j]=T[i];
+        j++;
+     } 
+    }
+      triA(temp,j);
+      affichCompte(temp,j);
+ }
+ void RtriD(Compte T[])
+ {
+    Compte temp[20];
+    int i, j = 0;
+    float nb ;
+    printf("enter le chiffre pivot: ");
+    scanf("%f",&nb);
+    for(i=0;i<num;i++)
+    {
+      if(T[i].montant >= nb)
+      {
+        temp[j]=T[i];
+        j++;
+     } 
+    }
+      triD(temp,j);
+      affichCompte(temp,j);
+ }
 /**************** menu principale *********************/
 int main ()
 {
-    int n,chose ;
+    int n,chose,p ;
     Compte T[n];
     int f,a=1;
     while(a)
@@ -103,6 +180,7 @@ int main ()
         puts("1) Ajouter un compte");
         puts("2) Ajouter plusieur compte");
         puts("3) Operation");
+        puts("4) Affichage");
         puts("====================================================");
         printf("entrez votre choix");
         scanf("%d",&f);
@@ -131,7 +209,34 @@ int main ()
                 depot(T,i);
                 break;
             }
+            break;
+            case 4:
+            puts("1)Tri Par Ordre Ascendant");
+            puts("2) Tri Par Ordre Descendant");
+            puts("3) Par Ordre Ascendant ayant un montant supérieur à un chiffre introduit");
+             puts("4) Par Ordre Descendant ayant un montant supérieur à un chiffre introduit");
+            printf("entrez votre type de tri");
+            scanf("%d",&p);
+            switch(p)
+            {
+                case 1:
+                triA(T,num);
+                affichCompte(T,num);
+                break;
+                case 2:
+                triD(T,num);
+                affichCompte(T,num);
+                break;
+               case 3:
+                RtriA(T);
+                break;
+                case 4:
+                RtriD(T);
+                break;
+            }
+            break;
         }
     }
     return 0;
 }
+
